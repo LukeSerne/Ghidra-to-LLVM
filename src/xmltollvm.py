ed822b888d7228e8a0b8570b332cded7eb7eb12b
Luke Serné <luke.serne@solconmail.nl>
2023-06-03 16:02:07 +0200

    Remove useless variables and make code more readable

--- src/xmltollvm.py
+++ src/xmltollvm.py
@@ -105,21 +105,15 @@ def populate_cfg(function, builders, blocks):
     stack_top = builder.gep(stack, [ir.Constant(int64, stack_size - 8)], name="stack_top")
     builder.store(stack_top, registers["RSP"])
     builder.branch(list(blocks.values())[1])
-    block_iterator = 1
-    instr = 0
-    quiter = False
-    for instruction in function.find("instructions"):
-        if quiter:
-            break
+
+    for block_iterator, instruction in enumerate(function.find("instructions"), start=2):
         address = instruction.find("address").text
-        if address in builders:
-            builder = builders[address]
-        pcodes = instruction.find("pcodes")
-        pc = 0
+        builder = builders.get(address, builder)
+
         no_branch = True
-        for pcode in pcodes:
-            pc += 1
+        for pcode in instruction.find("pcodes"):
             mnemonic = pcode.find("name")
+
             if mnemonic.text == "COPY":
                 output = pcode.find("output")
                 if output.text in flags and pcode.find("input_0").get("storage") == "constant":
@@ -127,6 +121,7 @@ def populate_cfg(function, builders, blocks):
                 else:
                     source = fetch_input_varnode(builder, pcode.find("input_0"))
                 update_output(builder, pcode.find("output"), source)
+
             elif mnemonic.text == "LOAD":
                 input_1 = pcode.find("input_1")
                 output = pcode.find("output")
@@ -139,15 +134,17 @@ def populate_cfg(function, builders, blocks):
                         rhs = builder.gep(rhs, [ir.Constant(int64, 0)])
                     result = builder.load(rhs)
                     update_output(builder, output, result)
+
             elif mnemonic.text == "STORE":
-                input_1 = pcode.find("input_1")  # target
-                input_2 = pcode.find("input_2")  # source
+                input_1 = pcode.find("input_1")  # store location
+                input_2 = pcode.find("input_2")  # store value
                 rhs = fetch_input_varnode(builder, input_2)
                 lhs = fetch_output_varnode(input_1)
                 lhs2 = builder.gep(lhs, [ir.Constant(int64, 0)])
                 if lhs2.type != rhs.type.as_pointer():
                     lhs2 = builder.bitcast(lhs2, rhs.type.as_pointer())
                 builder.store(rhs, lhs2)
+
             elif mnemonic.text == "BRANCH":
                 value = pcode.find("input_0").text[2:-2]
                 if value in functions:
@@ -161,26 +158,32 @@ def populate_cfg(function, builders, blocks):
                     # weird jump into some label in another function
                     # might be solved with callbr instruction?
                     builder.call(internal_functions["intra_function_branch"], [])
+
             elif mnemonic.text == "CBRANCH":
                 true_target = blocks[pcode.find("input_0").text[2:-2]]
-                false_target = list(blocks.values())[block_iterator + 1]
+                false_target = list(blocks.values())[block_iterator]
                 condition = fetch_input_varnode(builder, pcode.find("input_1"))
                 no_branch = False
                 builder.cbranch(condition, true_target, false_target)
+
             elif mnemonic.text == "BRANCHIND":
                 no_branch = False
                 target = fetch_input_varnode(builder, pcode.find("input_0"))
                 if not target.type.is_pointer:
                     target = builder.inttoptr(target, target.type.as_pointer())
                 builder.branch_indirect(target)
+
             elif mnemonic.text == "CALL":
                 target = functions[pcode.find("input_0").text[2:-2]][0]
                 builder.call(target, [])
+
             elif mnemonic.text == "CALLIND":
                 # target = pcode.find("input_0").text[2:-2]
                 builder.call(internal_functions["call_indirect"], [])
+
             elif mnemonic.text == "USERDEFINED":
                 raise NotImplementedError("The USERDEFINED operation cannot be implemented")
+
             elif mnemonic.text == "RETURN":
                 input_1 = pcode.find("input_1")
                 no_branch = False
@@ -188,6 +191,7 @@ def populate_cfg(function, builders, blocks):
                     builder.ret_void()
                 else:
                     raise NotImplementedError("RETURN operation that returns a value has not been implemented")
+
             elif mnemonic.text == "SUBPIECE":
                 output = pcode.find("output")
                 input_0 = pcode.find("input_0")
@@ -198,54 +202,63 @@ def populate_cfg(function, builders, blocks):
                     update_output(builder, output, result)
                 else:
                     builder.call(internal_functions['bit_extraction'], [])
+
             elif mnemonic.text == "INT_EQUAL":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 lhs, rhs = int_comparison_check_inputs(builder, lhs, rhs)
                 result = builder.icmp_unsigned('==', lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_NOTEQUAL":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 lhs, rhs = int_comparison_check_inputs(builder, lhs, rhs)
                 result = builder.icmp_unsigned('!=', lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_LESS":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 lhs, rhs = int_comparison_check_inputs(builder, lhs, rhs)
                 result = builder.icmp_unsigned('<', lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_SLESS":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 lhs, rhs = int_comparison_check_inputs(builder, lhs, rhs)
                 result = builder.icmp_signed('<', lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_LESSEQUAL":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 lhs, rhs = int_comparison_check_inputs(builder, lhs, rhs)
                 result = builder.icmp_unsigned('<=', lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_SLESS_EQUAL":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 lhs, rhs = int_comparison_check_inputs(builder, lhs, rhs)
                 result = builder.icmp_signed('<=', lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_ZEXT":
                 rhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 if rhs.type.is_pointer:
                     rhs = builder.ptrtoint(rhs, rhs.type.pointee)
                 output = builder.zext(rhs, ir.IntType(int(pcode.find("output").get("size")) * 8))
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_SEXT":
                 rhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 if rhs.type.is_pointer:
                     rhs = builder.ptrtoint(rhs, rhs.type.pointee)
                 output = builder.sext(rhs, ir.IntType(int(pcode.find("output").get("size")) * 8))
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_ADD":
                 input_0 = pcode.find("input_0")
                 input_1 = pcode.find("input_1")
@@ -258,18 +271,22 @@ def populate_cfg(function, builders, blocks):
                     lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                     result = builder.add(lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_SUB":
                 input_0 = pcode.find("input_0")
                 input_1 = pcode.find("input_1")
                 lhs = fetch_input_varnode(builder, input_0)
                 rhs = fetch_input_varnode(builder, input_1)
                 target = ir.IntType(int(pcode.find("output").get("size")) * 8)
+
                 if input_0.text in pointers and input_1.get("storage") == "constant":
                     result = builder.gep(lhs, [ir.Constant(int64, -int(input_1.text, 16))])
                 else:
                     lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                     result = builder.sub(lhs, rhs)
+
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_CARRY":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -277,6 +294,7 @@ def populate_cfg(function, builders, blocks):
                 result = builder.uadd_with_overflow(lhs, rhs)
                 result = builder.extract_value(result, 1)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_SCARRY":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -284,6 +302,7 @@ def populate_cfg(function, builders, blocks):
                 result = builder.sadd_with_overflow(lhs, rhs)
                 result = builder.extract_value(result, 1)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_SBORROW":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -291,14 +310,17 @@ def populate_cfg(function, builders, blocks):
                 result = builder.sadd_with_overflow(lhs, rhs)
                 result = builder.extract_value(result, 1)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_2COMP":
                 val = fetch_input_varnode(builder, pcode.find("input_0"))
                 result = builder.not_(val)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_NEGATE":
                 val = fetch_input_varnode(builder, pcode.find("input_0"))
                 result = builder.neg(val)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "INT_XOR":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -306,6 +328,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.xor(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_AND":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -313,6 +336,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.and_(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_OR":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -320,6 +344,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.or_(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_LEFT":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -327,6 +352,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = check_shift_inputs(builder, lhs, rhs, target)
                 output = builder.shl(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_RIGHT":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -334,6 +360,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = check_shift_inputs(builder, lhs, rhs, target)
                 output = builder.lshr(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_SRIGHT":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -341,6 +368,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = check_shift_inputs(builder, lhs, rhs, target)
                 output = builder.ashr(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_MULT":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -348,6 +376,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.mul(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_DIV":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -355,6 +384,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.div(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_REM":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -362,6 +392,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.urem(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_SDIV":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -369,6 +400,7 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.sdiv(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "INT_SREM":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
@@ -376,25 +408,30 @@ def populate_cfg(function, builders, blocks):
                 lhs, rhs = int_check_inputs(builder, lhs, rhs, target)
                 output = builder.srem(lhs, rhs)
                 update_output(builder, pcode.find("output"), output)
+
             elif mnemonic.text == "BOOL_NEGATE":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 result = builder.neg(lhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "BOOL_XOR":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 result = builder.xor(lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "BOOL_AND":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 result = builder.and_(lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text == "BOOL_OR":
                 lhs = fetch_input_varnode(builder, pcode.find("input_0"))
                 rhs = fetch_input_varnode(builder, pcode.find("input_1"))
                 result = builder.or_(lhs, rhs)
                 update_output(builder, pcode.find("output"), result)
+
             elif mnemonic.text in {
                     "FLOAT_EQUAL", "FLOAT_NOTEQUAL", "FLOAT_LESS", "FLOAT_LESSEQUAL",
                     "FLOAT_ADD", "FLOAT_SUB", "FLOAT_MULT", "FLOAT_DIV", "FLOAT_NEG",
@@ -404,11 +441,10 @@ def populate_cfg(function, builders, blocks):
                     "LZCOUNT", "PIECE", "POPCOUNT",
                 }:
                 raise NotImplementedError(f"PCODE opcode {mnemonic.text!r} is not implemented")
+
             else:
                 raise ValueError(f"{mnemonic.text!r} is not a standard pcode instruction")
 
-        block_iterator += 1
-        instr += 1
         if block_iterator < len(blocks) and no_branch:
             builder.branch(list(blocks.values())[block_iterator])
 
